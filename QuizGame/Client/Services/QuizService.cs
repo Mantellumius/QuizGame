55c3661cea7e6685132df3d83fd065de58179841
Mantellumius <mantellumius@gmail.com>
2023-02-18 06:03:58 +0500

    + Ability to edit quizzes

--- QuizGame/Client/Services/QuizService.cs
+++ QuizGame/Client/Services/QuizService.cs
@@ -25,18 +25,41 @@ public class QuizService
     private async Task Init()
     {
         var jsonWebToken = await _localStorageService.GetItemAsync<string>("bearerToken");
+        if (jsonWebToken is null) return;
         _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", jsonWebToken);
     }
 
-    public async Task<HttpResponseMessage> CreateQuiz(Quiz quiz)
+    public async Task<IEnumerable<Quiz>?> GetQuizzes()
+    {
+        return await _httpClient.GetFromJsonAsync<Quiz[]>("api/Quizzes");
+    }
+
+    public async Task<HttpResponseMessage> UpdateOrCreateQuiz(Quiz quiz)
+    {
+        if (quiz.Id == Guid.Empty)
+            return await CreateQuiz(quiz);
+        return await UpdateQuiz(quiz);
+    }
+
+    private async Task<HttpResponseMessage> UpdateQuiz(Quiz quiz)
+    {
+        return await _httpClient.PutAsJsonAsync($"/api/Quizzes/{quiz.Id}", quiz);
+    }
+
+    private async Task<HttpResponseMessage> CreateQuiz(Quiz quiz)
     {
         quiz.Id = Guid.NewGuid();
         quiz.AuthorId = await _authenticationStateProvider.GetUserGuid();
         return await _httpClient.PostAsJsonAsync("/api/Quizzes", quiz);
     }
 
-    public async Task<HttpResponseMessage> AddQuestions(Guid? quizId, List<Question> questions)
+    public async Task<HttpResponseMessage> SetQuestions(Guid? quizId, List<Question> questions)
     {
         return await _httpClient.PutAsJsonAsync($"/api/Quizzes/{quizId}/SetQuestions", questions);
+    }
+
+    public async Task<Quiz?> GetQuiz(string id)
+    {
+        return await _httpClient.GetFromJsonAsync<Quiz>($"api/Quizzes/{id}");
     }
 }
